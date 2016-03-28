75699f08aa39386fc1a4cb51ad2b58f1c5d21815
knucky <ambalV@uandex.ru>
2016-03-28 16:29:08 +0300

    Create toolbar.

--- app/src/main/java/com/sport/knucky/sportnotes/MainActivity.java
+++ app/src/main/java/com/sport/knucky/sportnotes/MainActivity.java
@@ -1,13 +1,36 @@
 package com.sport.knucky.sportnotes;
 
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends ActionBarActivity {
+
+    private Toolbar toolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolBar();
+    }
+
+    private void initToolBar() {
+        toolBar = (Toolbar) findViewById(R.id.toolBar);
+        toolBar.setTitle(R.string.app_name);
+        toolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        //setSupportActionBar(toolBar);
+
+        toolBar.inflateMenu(R.menu.menu);
     }
 }
